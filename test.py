38f4045a243700b1af40c512cf76803f5902815a
Denis <dbogdanov2007dush@gmail.com>
2026-07-11 15:12:03 +0700

    add TestClass

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import random
+from dataclasses import dataclass
 
 
 random.seed()
 
 
+@dataclass
+class TestClass:
+    random_num: int
+    title: str
+
+
 def get_random_num() -> int:
     return random.randint(1, 100)
+
+
+TestClass(get_random_num(), "from func: get_random_num")
+print(TestClass)
